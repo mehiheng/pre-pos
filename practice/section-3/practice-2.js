9240cc850f174f2b518f34f04c174af20a9f756d
mehiheng <38433021+mehiheng@users.noreply.github.com>
2018-07-04 00:52:08 +0800

    Update practice-2.js

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,20 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+ 	var result=Array();
+  for(var i=0;i<collectionA.length;i++)
+  {
+	  for(var n=0;n<objectB.value.length;n++)
+	  {
+		if(collectionA[i].key==objectB.value[n])
+		{
+			var j=collectionA[i].count;
+			var b=parseInt(j/3);
+			collectionA[i].count-=b;
+		}			
+		if(n==objectB.value.length-1)
+			{result.push(collectionA[i]);}
+	  }
+  }
+  return result;
 }
