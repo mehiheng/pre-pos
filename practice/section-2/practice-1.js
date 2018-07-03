48b5c03d6c76f61cd0a7ec6f026822fb0cade807
mehiheng <38433021+mehiheng@users.noreply.github.com>
2018-07-04 03:55:01 +0800

    Update practice-1.js

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,11 +1,28 @@
 'use strict';
 
 function countSameElements(collection) {
+  var result=Array();
+  var res=Array();
   for(var i=0;i<collection.length;i++)
   {
-	  for(var n=0;n<collection.lenth;n++)
+	  var j=0;
+	  for(var n=i;n<collection.length;n++)
 	  {
-		if(collection[i]) 
+		if(collection[i]==collection[n])
+		{
+			j++;
+		}			
 	  }
+	  res.push([collection[i],j]);  
+        i+=j;  
+		i=i-1;
   }
+  for(var a=0;a<res.length;a++)  
+{  
+var b=Object();
+b.key=res[a][0];
+b.count=res[a][1];
+ result.push(b);  
+}  
+  return result;
 }
