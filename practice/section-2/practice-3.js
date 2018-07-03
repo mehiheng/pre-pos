90c40cf84f40414616ff9099b06b40aaa8f12fbd
mehiheng <38433021+mehiheng@users.noreply.github.com>
2018-07-04 05:46:47 +0800

    unfinshed

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,41 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result=Array();
+  var res=Array();
+  collection.sort();
+  for(var i=0;i<collection.length;i++)
+  {
+	  var j=0;
+	   var re = /\W/;
+	  if( re.test(collection[i]) )
+	  {var temp=Array();
+       temp=collection[i].split("-");
+	    temp=collection[i].split("[");
+		temp=collection[i].split("]");
+		temp=collection[i].split(":");
+       collection[i]=temp[0];
+	   j=parseInt(temp[1]);
+	   res.push([collection[i],j]);
+      }
+	  j=0;
+	  for(var n=i;n<collection.length;n++)
+	  {
+		if(collection[i]==collection[n])
+		{
+			j++;
+		}			
+	  }
+	  res.push([collection[i],j]);  
+        i+=j;  
+		i=i-1;
+  }
+  for(var a=0;a<res.length;a++)  
+{  
+var b=Object();
+b.key=res[a][0];
+b.count=res[a][1];
+ result.push(b);  
+}  
+  return result;
 }
