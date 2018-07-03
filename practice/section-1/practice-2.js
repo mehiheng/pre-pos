d778bdab679402b102efc7abd1a262710df22a45
mehiheng <38433021+mehiheng@users.noreply.github.com>
2018-07-03 21:48:35 +0800

    Update practice-2.js

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+	var result=new Array();
+	for(var i=0;i<collectionA.length;i++)
+	{
+    for(var j=0;j<collectionB.length;j++)
+	{
+	for(var n=0;n<collectionB[j].length;n++)
+	{
+		if(collectionA[i]==collectionB[j][n])
+	{
+	 result.push(collectionA[i]);
+	}
+	}
+	}
+	}
+  return result;
 }
